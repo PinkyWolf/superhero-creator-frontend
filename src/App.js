c9c8c9db4c2a7b822f784f2c8633f88024170725
Serhii Hult <gyltsergiy@gmail.com>
2021-11-10 15:46:59 +0200

    added frontend part of proj

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import axios from "axios";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { data: [] };
+  }
+
+  componentDidMount() {
+    axios
+      .get("http://localhost:3000/api/heroes")
+      .then((res) => this.setState({ data: res.data }))
+      .catch((err) => err);
+  }
+
+  render() {
+    const { data } = this.state;
+    return (
+      <ul>
+        {Array.isArray(data) &&
+          data.map((obj) => (
+            <li>
+              <h1 className="heroName">{obj.nickname}</h1>
+              {/* <img src={} className="App-logo" alt="logo" /> */}
+              <p className="heroDescription">{obj}</p>
+              {/* <a>Learn React</a> */}
+            </li>
+          ))}
+      </ul>
+    );
+  }
 }
 
 export default App;
